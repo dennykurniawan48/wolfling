824f4903381f0684482ba2b742f8a0a472c7ae83
Denny Kurniawan <cipher.denny@gmail.com>
2023-11-28 14:00:41 +0700

    add request head to notification route to get notification count

--- app/api/notification/route.ts
+++ app/api/notification/route.ts
@@ -4,7 +4,7 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest } from "next/server";
 const jwt = require("jsonwebtoken")
 
-export async function GET(req: NextRequest) {
+export async function HEAD(req: NextRequest) {
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET, raw: true })
     const user = jwt.verify(token, process.env.NEXTAUTH_SECRET)
     if (user) {
@@ -16,5 +16,56 @@ export async function GET(req: NextRequest) {
         })
 
         return Response.json({ data: { count: count } })
+    }
+}
+
+export async function GET(req: NextRequest) {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET, raw: true })
+    const user = jwt.verify(token, process.env.NEXTAUTH_SECRET)
+    if (user) {
+        const notification = await prisma.notification.findMany({
+            where: {
+              userTo: user?.user.id,
+            },
+            include: {
+              DestinationUser: {
+                select: {
+                  id: true,
+                  name: true,
+                  username: true,
+                  image: true,
+                },
+              },
+              OriginUser: {
+                select: {
+                  id: true,
+                  name: true,
+                  username: true,
+                  image: true,
+                },
+              },
+            },
+          });
+
+        return Response.json({ data: notification })
+    }
+}
+
+export async function PUT(req: NextRequest){
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET, raw: true })
+    const user = jwt.verify(token, process.env.NEXTAUTH_SECRET)
+    if (user) {
+        await prisma.notification.updateMany({
+            where: {
+                userTo: user.id,
+                opened: false
+            }, data:{
+                opened: true
+            }
+        })
+
+        pusherServer.trigger(toPusherKey(`user:${user.id}:notification`), "clear_notification", 0)
+
+        return Response.json({ data: { cleared: true } })
     }
 }
