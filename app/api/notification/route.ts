c2c9ce2df556fa8451d6a9608dc5d0b0a8977f81
Denny Kurniawan <cipher.denny@gmail.com>
2023-11-28 14:17:20 +0700

    remove put request from notification route

--- app/api/notification/route.ts
+++ app/api/notification/route.ts
@@ -18,22 +18,3 @@ export async function GET(req: NextRequest) {
         return Response.json({ data: { count: count } })
     }
 }
-
-export async function PUT(req: NextRequest){
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET, raw: true })
-    const user = jwt.verify(token, process.env.NEXTAUTH_SECRET)
-    if (user) {
-        await prisma.notification.updateMany({
-            where: {
-                userTo: user.id,
-                opened: false
-            }, data:{
-                opened: true
-            }
-        })
-
-        await pusherServer.trigger(toPusherKey(`user:${user.id}:notification`), "clear_notification", 0)
-
-        return Response.json({ data: { cleared: true } })
-    }
-}
